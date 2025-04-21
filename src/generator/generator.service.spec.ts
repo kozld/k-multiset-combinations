5157732d3b8c65c21c1659ecd54444c215f5d270
dydx <denis@tonbet.fun>
2025-04-21 21:51:44 +0400

    bugfix/unit-tests: broken tests fixed

--- src/generator/generator.service.spec.ts
+++ src/generator/generator.service.spec.ts
@@ -1,12 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { GeneratorService } from './generator.service';
 import { CombinatoryService } from '../combinatory/combinatory.service';
+import { DatabaseService } from '../database/database.service';
+import { DatabaseProvider } from '../database/database.provider';
 
 describe('GeneratorService', () => {
     let service: GeneratorService;
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            providers: [GeneratorService, CombinatoryService],
+            providers: [GeneratorService, CombinatoryService, DatabaseService, DatabaseProvider],
         }).compile();
 
         service = module.get<GeneratorService>(GeneratorService);
